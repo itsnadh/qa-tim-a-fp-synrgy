b05af30e19d0b9681c3f4af89a9a4f45cb94ebdb
TiaraKesuma <tiara.kesuma.p@mail.ugm.ac.id>
2023-01-25 20:49:55 +0700

    add login and logout for 2 roles in chrome -  automation testing

--- Selenium_FinalProject_QA/src/test/java/testCase/LoginTest.java
+++ Selenium_FinalProject_QA/src/test/java/testCase/LoginTest.java
@@ -1,6 +1,8 @@
 package testCase;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,49 +10,87 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+import page.LoginPage;
 
 import java.time.Duration;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class LoginTest {
 
     WebDriver driver;
 
-//    @BeforeTest
-//    @Parameters("browser")
-//    public void setup(String browser) {
-//        System.out.println("==========Browser is launching==========");
-//        if (browser.equalsIgnoreCase("Chrome")) {
-//            ChromeOptions options = new ChromeOptions();
-//            WebDriverManager.chromedriver().setup();
-//            options.addArguments("--headless");
-//            driver = new ChromeDriver(options);
-//            driver.manage().window().maximize();
-//            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-//        } else if (browser.equalsIgnoreCase("Edge")) {
-//            EdgeOptions options = new EdgeOptions();
-//            options.addArguments("--headless");
-//            WebDriverManager.edgedriver().setup();
-//            driver = new EdgeDriver(options);
-//            driver.manage().window().maximize();
-//            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-//        } else if (browser.equalsIgnoreCase("Firefox")) {
-//            FirefoxOptions options = new FirefoxOptions();
-//            WebDriverManager.firefoxdriver().setup();
-//            options.addArguments("--headless");
-//            driver = new FirefoxDriver(options);
-//            driver.manage().window().maximize();
-//            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-//        }
-//    }
-//
-//    @Test(priority = 1)
-//    public void loginSuccess(){
-//
-//    }
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+    @BeforeTest
+    public void setup() {
+        System.out.println("==========Browser is launching==========");
+        driver = WebDriverManager.chromedriver().create();
+        driver.get("https://frontend-fsw-testing.vercel.app/login");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(8, SECONDS);
+    }
 
+    @Test(priority = 1)
+    public void loginAsSeeker() throws InterruptedException {
+        LoginPage loginPage = new LoginPage(driver);
+        Thread.sleep(2000);
+        assertEquals(driver.getCurrentUrl(), "https://frontend-fsw-testing.vercel.app/login");
+        System.out.println("Sukses buka link login");
+        loginPage.loginAsSeeker();
+        assertEquals(driver.getCurrentUrl(), "https://frontend-fsw-testing.vercel.app/login/pencari");
+        System.out.println("Sukses buka link login seeker");
+        Thread.sleep(3000);
+        loginPage.inputEmail("seeker@mail.com");
+        loginPage.inputPassword("password");
+        loginPage.clickButtonLogin();
+        Thread.sleep(5000);
+        assertEquals(driver.getCurrentUrl(), "https://frontend-fsw-testing.vercel.app/");
+        Thread.sleep(2000);
+    }
 
+    @Test(priority = 2)
+    public void loginAsTenant() throws InterruptedException{
+        LoginPage loginPage = new LoginPage(driver);
+        Thread.sleep(2000);
+        assertEquals(driver.getCurrentUrl(), "https://frontend-fsw-testing.vercel.app/login");
+        System.out.println("Sukses buka link login");
+        loginPage.loginAsTenant();
+        assertEquals(driver.getCurrentUrl(), "https://frontend-fsw-testing.vercel.app/login/penyewa");
+        System.out.println("Sukses buka link login tenant");
+        Thread.sleep(3000);
+        loginPage.inputEmail("tennant@mail.com");
+        loginPage.inputPassword("password");
+        loginPage.clickButtonLogin();
+        Thread.sleep(5000);
+        assertEquals(driver.getCurrentUrl(), "https://frontend-fsw-testing.vercel.app/penyewa");
+        Thread.sleep(2000);
+    }
 
+    @Test(priority = 3)
+    public void logOutFromSeeker() throws InterruptedException{
+        LoginPage loginPage = new LoginPage(driver);
+        loginAsSeeker();
+        loginPage.logOut();
+        Thread.sleep(3000);
+        assertEquals(driver.getCurrentUrl(), "https://frontend-fsw-testing.vercel.app/");
+    }
+
+    @Test(priority = 4)
+    public void logOutFromTenant() throws InterruptedException{
+        LoginPage loginPage = new LoginPage(driver);
+        loginAsTenant();
+        loginPage.logOut();
+        Thread.sleep(3000);
+        assertEquals(driver.getCurrentUrl(), "https://frontend-fsw-testing.vercel.app/");
+    }
+    @AfterTest
+    public void quit(){
+        driver.quit();
+    }
 }
